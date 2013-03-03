da2b5ee31b89b4ff372d37cdb67be80c6420af7b
Alfasado Inc <webmaster@alfasado.jp>
2013-03-03 15:34:56 +0900

    first commit

--- plugins/AssetThumbnailMagick/lib/AssetThumbnailMagick/L10N/ja.pm
+++ plugins/AssetThumbnailMagick/lib/AssetThumbnailMagick/L10N/ja.pm
@@ -4,7 +4,7 @@ use base 'AssetThumbnailMagick::L10N';
 use vars qw( %Lexicon );
 
 our %Lexicon = (
-    'Cutting a circle out of an image.' => '画像を円形に切り抜きます。',
+    'It processes the thumbnail image using ImageMagick.' => 'ImageMagickを利用してサムネイル画像を加工します。',
  );
 
 1;
